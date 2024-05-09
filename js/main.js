8ae5d013b899d8ac83f5b9547e57f52dd59df16c
RosyDiVilio <rosydivilio@gmail.com>
2024-05-09 14:08:51 +0200

    add: completed exercise

--- js/main.js
+++ js/main.js
@@ -3,16 +3,18 @@
 const kmNumber = prompt('Inserisci i km che devi percorrere per arrivare a destinazone');
 const age = prompt('Inserisci la tua eta');
 
-let molt = kmNumber * parseFloat(0.21); 
+let molt = kmNumber * parseFloat(0.21);
+let discount20 = (molt * 20) / 100;
+let discount40 = (molt * 40) / 100;
 
 if (age < 18) {
-    console.log ('hai diritto ad uno sconto del 20%');
+    console.log ('il cliente ha diritto ad uno sconto del 20%');
+    document.getElementById('ticket').innerHTML = molt - discount20.toFixed(2);
 } else if (age > 65) {
-    console.log ('hai diritto ad uno sconto del 40%');
+    console.log ('il cliente ha diritto ad uno sconto del 40%');
+    document.getElementById('ticket').innerHTML = molt - discount40.toFixed(2);
 } else {
     console.log ('paga prezzo pieno');
 }
 
 console.log(molt);
-
-document.getElementById('ticket').innerHTML = molt.toFixed(2);
