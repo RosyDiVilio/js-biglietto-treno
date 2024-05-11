e54bdf95510c9471a113b979412f992a6648af8f
RosyDiVilio <rosydivilio@gmail.com>
2024-05-11 15:06:00 +0200

    add: done

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ let discount20 = (molt * 20) / 100;
 let discount40 = (molt * 40) / 100;
 
 if (age < 18) {
-    console.log ('il cliente ha diritto ad uno sconto del 20%');
-    document.getElementById('ticket').innerHTML = molt - discount20.toFixed(2);
+    console.log('il cliente ha diritto ad uno sconto del 20%');
+    document.getElementById('ticket').innerHTML = (molt - discount20).toFixed(2);
 } else if (age > 65) {
-    console.log ('il cliente ha diritto ad uno sconto del 40%');
-    document.getElementById('ticket').innerHTML = molt - discount40.toFixed(2);
+    console.log('il cliente ha diritto ad uno sconto del 40%');
+    document.getElementById('ticket').innerHTML = (molt - discount40).toFixed(2);
 } else {
-    console.log ('paga prezzo pieno');
+    console.log('paga prezzo pieno');
     document.getElementById('ticket').innerHTML = molt.toFixed(2);
 }
 
